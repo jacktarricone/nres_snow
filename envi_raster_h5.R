6cca6a8527a49ea128281ad47769192728c0dfe9
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2021-11-09 15:21:47 -0800

    envi to SWE code, works well

--- envi_raster_h5.R
+++ envi_raster_h5.R
@@ -16,52 +16,79 @@ library(rhdf5)
 envi_years <-file.path("/Volumes","jack_t","nres_project","reg_years")
 years_path <-list.files(envi_years, full.names = TRUE) # create path to each year
 years <-list.files(envi_years) # just a years list for saving file path purposes
+  
+# testing
+year <-years[[5]]
+
+envi_to_hdf5 <-function(year){
 
 # list .dat and .hdr files in each year, just 2004 to test
-dat_files <-list.files(years_path[2], pattern = ".dat", full.names = TRUE)
-hdr_files <-list.files(years_path[2], pattern = ".hdr", full.names = TRUE)
+dat_files <-list.files(file.path("/Volumes","jack_t","nres_project","reg_years", year), 
+                       pattern = ".dat", full.names = TRUE)
+hdr_files <-list.files(file.path("/Volumes","jack_t","nres_project","reg_years", year), 
+                       pattern = ".hdr", full.names = TRUE)
 
-mat_list <- vector(mode = "list", length = length(dat_files)) # create empty list to loop into
+# create empty list to loop into
+mat_list <- vector(mode = "list", length = length(dat_files))
 
 # loop to read in data matrixes
 system.time(for (i in 1:length(dat_files)){
   mat_list[[i]] <-read.ENVI(dat_files[i], hdr_files[i]) 
 })
 
+## define function to crop to western us
+# 3351, 2488, 1 crops it to -104 lon or just east of denver
+wu_crop <-function(x){x[,1:2488]}
+
+# crop matrix list before making data cubes
+system.time(mat_list <-lapply(mat_list, wu_crop))
+
+# define dimensions 
+nlines <-3351 # lat
+nsamps <-2488 # lon orginally 6935
+
+# use length of list to define to half chunks
+# code was breaking when trying to convert whole list to array
+# first_half <-as.integer(length(mat_list)/2)
+  
 # transform list of matrixes into large array aka datacube
-system.time(swe_array <-array(as.numeric(unlist(mat_list)), dim=c(3351, 6935, length(mat_list))))
+system.time(swe_array <-array(as.numeric(unlist(mat_list)), dim=c(nlines, nsamps, length(mat_list))))
 rm(mat_list)
 system.time(swe_array[swe_array == -9999] <- NA) # change no data value to NA
 system.time(swe_array <-swe_array/1000) # divide to get units meters
-
-## crop to western us
-# 3351, 2488, 1 crops it to -104 lon or just east of denver
-swe_array <-swe_array[,1:2488,]
-
-# info for converting to rasters
-
-# ext(swe_stack) <-c(-124.7337, -104.0004, 24.9504, 52.8754)
-# crs(swe_stack) <-"+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs" # set proj
 
 ############################
 ##### save to h5 file ######
 ############################
 
 # setwd , change for function
-setwd(file.path("/Volumes","jack_t","nres_project","reg_years","annual_h5")) 
+setwd(file.path("/Volumes","jack_t","nres_project","hdf5_reg_years")) 
 
-h5createFile("swe_daily_2004.h5") # create empty .h5 file
+hdf_file_name <-paste0("swe_daily_",year,".h5")
+
+h5createFile(hdf_file_name) # create empty .h5 file
 
 # create data set with proper info, set "chunk"
 # level is compression amount 0 - 9, 9 is most compressed but slowest to read in
-h5createDataset(file = "swe_daily_2004.h5", dataset = "swe",
-                dims = c(3351, 2488, length(dat_files)), level = 5, chunk = c(3351, 2488, length(dat_files)),
+h5createDataset(file = paste0("swe_daily_",year,".h5"), dataset = "swe",
+                dims = c(nlines, nsamps, length(dat_files)), level = 5, chunk = c(nlines, nsamps, length(dat_files)),
                 storage.mode = "double")
 
 # view the structure of the h5 we've created
-h5ls("swe_daily_2004.h5")
+h5ls(hdf_file_name)
 
-h5write(swe_array, "swe_daily_2004.h5","swe")
+# write data the the hdf5 file we made
+h5write(swe_array, hdf_file_name,"swe")
+rm(swe_array)
+
+}
+
+
+
+# info for converting to rasters
+
+# ext(swe_stack) <-c(-124.7337, -104.0004, 24.9504, 52.8754)
+# crs(swe_stack) <-"+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs" # set proj
 
 
 #########################################
