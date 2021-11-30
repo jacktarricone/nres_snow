c389a61b54357c80077a8c389fae331f1828f147
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2021-11-30 14:44:15 -0800

    updated mk test on raster stack for snodas

---
+++ max_swe_mk_jack.R
@@ -0,0 +1,69 @@
+# snodas max swe mk test
+
+
+# november 30th 2021
+# jack tarricone
+
+library(raster)
+library(rgdal)
+library(parallel)
+library(spatialEco)
+library(EnvStats)
+library(Kendall)
+library(snow)
+
+
+# read in max_swe snodas rasster
+setwd("/Users/jacktarricone/Desktop/max_swe")
+list <-list.files()
+max_list <-lapply(list, function(x) raster(x))
+max_stack <-stack(max_list) 
+plot(max_stack[[9]])
+max_stack
+
+
+###### mk test with just pval and slope estimate
+
+# updated trend.slope function from the spatialEco::raster.kendall 
+# updated Envstats:kendallTrendTest function to ci.slope = FALSE to hopefully suppress error
+
+trend.slope2 <- function(y, tau.pass = FALSE, p.value.pass = TRUE,  
+                        confidence.pass = FALSE, z.value.pass = FALSE,
+                        intercept.pass = FALSE) {
+  fit <- suppressWarnings( EnvStats::kendallTrendTest(y ~ 1, ci.slope = FALSE) ) # important, error before this
+  fit.results <- fit$estimate[2]
+  if(p.value.pass == TRUE) { fit.results <- c(fit.results, fit$p.value) } 
+  if(z.value.pass == TRUE) { fit.results <- c(fit.results, fit$statistic) } 
+  if(confidence.pass == TRUE) { 
+    ci <- unlist(fit$interval["limits"])
+    if( length(ci) == 2) { 
+      fit.results <- c(fit.results, ci)
+    } else {
+      fit.results <- c(fit.results, c(NA,NA))
+    }			  
+  }
+  if(intercept.pass == TRUE) { fit.results <- c(fit.results, fit$estimate[3]) }  
+  if(tau.pass == TRUE) { fit.results <- c(fit.results, fit$estimate[1]) }  
+  return( fit.results )
+}
+
+
+# run it in parallel using snow and parallel 
+beginCluster(n=12) # number of cores you want, never use them all!
+
+system.time(max_trends_full <- raster::clusterR(max_stack, overlay, args=list(fun=trend.slope2)))
+
+endCluster()
+
+
+# pull out the data
+p_value <-max_trends_full[[2]]
+slope <-max_trends_full[[1]]
+
+# test plot
+plot(p_value) # p_val
+plot(slope) # trend
+
+# save
+writeRaster(p_value,"/Volumes/jt/projects/margulis/snow_metric_rasters/max_swe/mk_results/max_swe_p_value.tif")
+writeRaster(slope, "/Volumes/jt/projects/margulis/snow_metric_rasters/max_swe/mk_results/max_swe_slope.tif")
