fcf4f5733cc8f848967c480aa6be737c2eb20188
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2021-11-30 15:11:08 -0800

    complete script of mk test and saving all four rasters

--- max_swe_mk_jack.R
+++ max_swe_mk_jack.R
@@ -49,11 +49,18 @@ trend.slope2 <- function(y, tau.pass = FALSE, p.value.pass = TRUE,
 
 
 # run it in parallel using snow and parallel 
+
 beginCluster(n=12) # number of cores you want, never use them all!
 
 system.time(max_trends_full <- raster::clusterR(max_stack, overlay, args=list(fun=trend.slope2)))
 
 endCluster()
+
+
+
+###########################
+##### raster creation #####
+##########################
 
 
 # pull out the data
@@ -64,6 +71,17 @@ slope <-max_trends_full[[1]]
 plot(p_value) # p_val
 plot(slope) # trend
 
+# create raster for just significant p-values
+sig_pval <-p_value
+values(sig_pval)[values(sig_pval) > 0.05] = NA
+plot(sig_pval)
+
+# mask slope raster with sig p-values
+sig_slope <-mask(slope, sig_pval)
+plot(sig_slope)
+
 # save
-writeRaster(p_value,"/Volumes/jt/projects/margulis/snow_metric_rasters/max_swe/mk_results/max_swe_p_value.tif")
-writeRaster(slope, "/Volumes/jt/projects/margulis/snow_metric_rasters/max_swe/mk_results/max_swe_slope.tif")
+writeRaster(sig_pval, "/Volumes/JT/2021_1_fall_UNR/eco_r/mk_results/max_swe_sig_pval.tif")
+writeRaster(p_value,"/Volumes/JT/2021_1_fall_UNR/eco_r/mk_results/max_swe_p_value.tif")
+writeRaster(sig_slope, "/Volumes/JT/2021_1_fall_UNR/eco_r/mk_results/max_swe_sig_slope.tif")
+writeRaster(slope, "/Volumes/JT/2021_1_fall_UNR/eco_r/mk_results/max_swe_slope.tif")
